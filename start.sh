cdf90cf0edb9741b9816b08958089e559638d9c5
Juan Camilo Florez <juan.florez@prodigious.com>
2015-08-10 16:49:23 -0500

    modificacion de los archivos de instalacion y configuracion

--- start.sh
+++ start.sh
@@ -6,11 +6,14 @@ elif [ $# -gt 1 ]; then
 	echo "Only one argument needed. Possible arguments: [ubuntu|macosx]."
 elif [ $1 == "ubuntu" ] || [ $1 == "macosx" ]; then
 	if [ $1 == "ubuntu" ]; then
-  		exec /home/diewebsiten/DieWebsitenInstallation/ftpserver/ubuntu/vsftpd.sh
+  		exec /home/diewebsiten/DieWebsitenInstallation/ftpserver/$1/vsftpd.sh
+  		exec /home/diewebsiten/DieWebsitenInstallation/java/$1/java.sh
+		exec /home/diewebsiten/DieWebsitenInstallation/cassandra/$1/cassandra.sh
+		exec /home/diewebsiten/DieWebsitenInstallation/tomcat/$1/tomcat.sh
   	fi
-	exec /home/diewebsiten/DieWebsitenInstallation/java/$1/java.sh
-	exec /home/diewebsiten/DieWebsitenInstallation/cassandra/$1/cassandra.sh
-	exec /home/diewebsiten/DieWebsitenInstallation/tomcat/$1/tomcat.sh
+	exec ~/DieWebsitenInstallation/java/$1/java.sh
+	exec ~/DieWebsitenInstallation/cassandra/$1/cassandra.sh
+	exec ~/DieWebsitenInstallation/tomcat/$1/tomcat.sh
 else
   	echo $1
   	echo "is an invalid argument. Possible arguments: [ubuntu|macosx]"
